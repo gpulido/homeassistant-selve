2bc920a783375ac60b0db6e6c47b3fee094cae27
gpulido <gabriel.pulidodetorres@gmail.com>
2023-06-06 00:07:33 +0200

    Sort manifest. Add pytest

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -1,10 +1,12 @@
 """Tests for the config flow."""
 from unittest import mock
 
+import pytest
+
 from custom_components.selve import config_flow
 from custom_components.selve.const import DOMAIN
 
-
+@pytest.mark.asyncio
 async def test_flow_user_init(hass):
     """Test the initialization of the form in the first step of the config flow."""
     result = await hass.config_entries.flow.async_init(
